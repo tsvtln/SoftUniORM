a6cae75014675d9e5a11357128599bcc9403f9b5
tsvtln <tsvtln@abv.bg>
2024-06-25 17:23:31 +0300

    05_product

--- 01_Django_Models_Basics/Exercise/main_app/models.py
+++ 01_Django_Models_Basics/Exercise/main_app/models.py
@@ -15,3 +15,36 @@ class Blog(m.Model):
         max_length=35
     )
 
+
+class WeatherForecast(m.Model):
+    date = m.DateField()
+    temperature = m.FloatField()
+    humidity = m.FloatField()
+    precipitation = m.FloatField()
+
+
+class Recipe(m.Model):
+    name = m.CharField(
+        max_length=100,
+        unique=True
+    )
+    description = m.TextField()
+    ingredients = m.TextField()
+    cook_time = m.PositiveIntegerField()
+    created_at = m.DateTimeField(
+        auto_now_add=True
+    )
+
+
+class Product(m.Model):
+    name = m.CharField(
+        max_length=70
+    )
+    description = m.TextField()
+    price = m.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    created_at = m.DateTimeField(
+        auto_now_add=True
+    )
