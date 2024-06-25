f8eb51231ccae1cfe50c33e20edaf6f95ac8af06
tsvtln <tsvtln@abv.bg>
2024-06-25 15:44:54 +0300

    04

--- 01_Django_Models_Basics/Lab/03_migrate_the_models/main_app/models.py
+++ 01_Django_Models_Basics/Lab/03_migrate_the_models/main_app/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 
@@ -35,6 +37,54 @@ class Department(models.Model):
     location = models.CharField(
         max_length=20,
         choices=Locations
+    )
+
+    last_edited_on = models.DateTimeField(
+        auto_now=True,
+        editable=False
+    )
+
+
+class Project(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    budget = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+
+    duration_in_days = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        verbose_name='Duration in Days'
+    )
+
+    estimated_hours = models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name='Estimated Hours'
+    )
+
+    start_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Start Date',
+        default=date.today()
+    )
+
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
     )
 
     last_edited_on = models.DateTimeField(
