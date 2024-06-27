49ce97000d1c849986f320f5a53b1de55fefcbfe
tsvtln <tsvtln@abv.bg>
2024-06-27 15:12:38 +0300

    packpy

--- 01_Django_Models_Basics/Exercise/main_app/models.py
+++ 01_Django_Models_Basics/Exercise/main_app/models.py
@@ -3,7 +3,7 @@ from django.db import models as m
 
 class Person(m.Model):
     name = m.CharField(
-        max_length=30
+        max_length=30,
     )
 
     age = m.PositiveIntegerField()
@@ -12,7 +12,7 @@ class Person(m.Model):
 class Blog(m.Model):
     post = m.TextField()
     author = m.CharField(
-        max_length=35
+        max_length=35,
     )
 
 
@@ -26,49 +26,73 @@ class WeatherForecast(m.Model):
 class Recipe(m.Model):
     name = m.CharField(
         max_length=100,
-        unique=True
+        unique=True,
     )
     description = m.TextField()
     ingredients = m.TextField()
     cook_time = m.PositiveIntegerField()
     created_at = m.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
     )
 
 
 class Product(m.Model):
     name = m.CharField(
-        max_length=70
+        max_length=70,
     )
     description = m.TextField()
     price = m.DecimalField(
         max_digits=10,
-        decimal_places=2
+        decimal_places=2,
     )
     created_at = m.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
     )
 
 
 class UserProfile(m.Model):
     username = m.CharField(
         max_length=65,
-        unique=True
+        unique=True,
     )
     first_name = m.CharField(
-        max_length=40
+        max_length=40,
     )
     last_name = m.CharField(
-        max_length=40
+        max_length=40,
     )
     email = m.EmailField(
         unique=True,
-        default='students@softuni.bg'
+        default='students@softuni.bg',
     )
     bio = m.TextField(
-        max_length=120
+        max_length=120,
     )
     profile_image_url = m.URLField()
     created_at = m.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+    )
+
+
+class Exercise(m.Model):
+    name = m.CharField(
+        max_length=100,
+    )
+    description = m.TextField()
+    difficulty_level = m.CharField(
+        max_length=20,
+    )
+    duration_minutes = m.PositiveIntegerField()
+    equipment = m.CharField(
+        max_length=90,
+    )
+    video_url = m.URLField(
+        null=True,
+        blank=True,
+    )
+    calories_burned = m.PositiveIntegerField(
+        default=1,
+    )
+    is_favorite = m.BooleanField(
+        default=False,
     )
