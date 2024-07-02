847362887b56157b739ec670ad2b985dcc13b5cf
tsvtln <tsvtln@abv.bg>
2024-07-02 14:03:36 +0300

    01_02_03

---
+++ 02_Migrations_and_Django_Admin/Exercise/main_app/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class Shoe(models.Model):
+    brand = models.CharField(
+        max_length=25,
+    )
+
+    size = models.PositiveIntegerField()
+
+
+class UniqueBrands(models.Model):
+    brand_name = models.CharField(
+        max_length=25,
+        unique=True,
+    )
+
+
+class EventRegistration(models.Model):
+    event_name = models.CharField(
+        max_length=60
+    )
+    participant_name = models.CharField(
+        max_length=50
+    )
+    registration_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.participant_name} - {self.event_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(
+        max_length=100
+    )
+    director = models.CharField(
+        max_length=100
+    )
+    release_year = models.PositiveIntegerField()
+    genre = models.CharField(
+        max_length=50
+    )
+
+    def __str__(self):
+        return f"Movie \"{self.title}\" by {self.director}"
+
