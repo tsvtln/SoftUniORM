72cd45d94270c490921c3d85b1a01b54ca112513
tsvtln <tsvtln@abv.bg>
2024-07-02 16:29:30 +0300

    06_course

--- 02_Migrations_and_Django_Admin/Exercise/main_app/admin.py
+++ 02_Migrations_and_Django_Admin/Exercise/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main_app.models import EventRegistration, Movie, Student, Supplier
+from main_app.models import EventRegistration, Movie, Student, Supplier, Course
 
 
 @admin.register(EventRegistration)
@@ -99,3 +99,57 @@ class SupplierAdmin(admin.ModelAdmin):
             )
         }),
     )
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'lecturer',
+        'price',
+        'start_date',
+    )
+
+    list_filter = (
+        'is_published',
+        'lecturer',
+    )
+
+    search_fields = (
+        'title',
+        'lecturer',
+    )
+
+    readonly_fields = (
+        'start_date',
+    )
+
+    fieldsets = (
+        ('Course Information',
+            {'fields': (
+                'title',
+                'lecturer',
+                'price',
+                'start_date',
+                'is_published',
+            )
+            }
+         ),
+        ('Description',
+            {'fields': (
+                'description',
+            )
+            }))
+
+
+
+
+
+
+
+
+
+
+
+
+
