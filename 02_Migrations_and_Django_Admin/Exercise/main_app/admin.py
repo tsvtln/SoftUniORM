f9fa72ed0386b5fd4be315354a098e159b6b4494
tsvtln <tsvtln@abv.bg>
2024-07-02 14:14:52 +0300

    04

--- 02_Migrations_and_Django_Admin/Exercise/main_app/admin.py
+++ 02_Migrations_and_Django_Admin/Exercise/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main_app.models import EventRegistration, Movie
+from main_app.models import EventRegistration, Movie, Student
 
 
 @admin.register(EventRegistration)
@@ -21,6 +21,7 @@ class EventRegistrationAdmin(admin.ModelAdmin):
         'participant_name',
     )
 
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = (
@@ -36,4 +37,34 @@ class MovieAdmin(admin.ModelAdmin):
     search_fields = (
         'title',
         'director',
+    )
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'age',
+        'grade',
+    )
+
+    list_filter = (
+        'age',
+        'grade',
+        'date_of_birth',
+    )
+
+    search_fields = (
+        'first_name',
+    )
+
+    fieldsets = (
+        ('Personal Information', {
+            'fields': ('first_name', 'last_name', 'age', 'date_of_birth'),
+        }),
+
+        ('Academic Information', {
+            'fields': ('grade',)
+        }),
     )
